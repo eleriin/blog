4799d41ca5bd6121f34acfee0c242a34b0360599
eleriin <eleriin.kurs@voco.ee>
2026-03-26 12:48:03 +0200

    working app

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import PostList from './PostList'
+import PostCreate from './PostCreate'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return(
+    <div className='container'>
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  )
+} 
+
+export default App;
